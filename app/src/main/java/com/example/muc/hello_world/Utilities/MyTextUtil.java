4cdde7e2e344a9f14aac00ba5363666bb39f6075
Parker Lee <hla12345678910@gmail.com>
2015-05-20 22:53:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/muc/hello_world/Utilities/MyTextUtil.java
@@ -0,0 +1,11 @@
+package com.example.muc.hello_world.Utilities;
+
+/**
+ * Created by Mu.C on 5/20/2015.
+ */
+public class MyTextUtil {
+
+    public String getVal(){
+        return "Hello from the utility class 123932u42398";
+    }
+}
